177d269b1134b337d2134d02e181402732542ba0
hssnhlim <hassanhalim52@gmail.com>
2026-02-26 23:44:08 +0800

    add request mapping

--- auth-service/src/main/java/com/pm/authservice/controller/AuthController.java
+++ auth-service/src/main/java/com/pm/authservice/controller/AuthController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Optional;
 
 @RestController
+@RequestMapping("/api/auth")
 public class AuthController {
 
     private final AuthService authService;
